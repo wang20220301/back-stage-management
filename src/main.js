2fc9da8e444f2fb9c8a507ab3f8a463dcbb5cf00
wangshaochuan <1738376632@qq.com>
2022-03-02 17:07:31 +0800

    完成了ui框架的搭建

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from "./vuex/index"
 import router from "./router/index"
-
+import ElementUI from 'element-ui';
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
   store,
   router,
