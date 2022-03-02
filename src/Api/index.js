fe1250b4c8d86fe53e4ccf9d8b23b8946ddd2683
wangshaochuan <1738376632@qq.com>
2022-03-02 16:15:23 +0800

    完成了路由和vuex以及axios搭建

---
+++ src/Api/index.js
@@ -0,0 +1,46 @@
+
+// 引入axios;
+import axios from "axios";
+
+// 创建axios实例
+const my = axios.create({
+  // 此地址上线之后要进行修改
+  // 这个地址就是请求发生地址
+  baseURL: "http://localhost:3306",
+});
+
+// 创建get提交方法
+
+/** 
+@param {function} get   方法名
+@param {string}  path   发送请求的uri地址
+@param {object}  params  请求的参数
+@param {object} config  配置参数 
+*/
+
+// get的params传参是以对象的方式传递
+function get(path, params, config = {}) {
+  return my.get(path, {
+    params,
+    ...config,
+  })
+}
+
+// 创建post方法
+
+/** 
+@param {function} post  方法名
+@param {string}  path   发送请求的uri地址
+@param {Array}  data  请求的参数
+@param {object} config  配置参数 
+*/
+
+
+function post(path, data = {}, config = {}) {
+  return my.post(path, data, config)
+}
+
+export {
+  get,
+  post
+}
---
+++ src/components/login/login.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div>
+        hahahahahaha
+        <router-view></router-view>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:"login-a"
+}
+</script>
+<style>
+
+</style>
